5a7b5826cf5603eeafebd798da2655906a7e5483
Alien551 <84018879+Alien551@users.noreply.github.com>
2023-05-06 20:36:23 +0300

    Add files via upload

---
+++ Simple Runner/Form1.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Timer1 = New Timer(components)
+
+        Timer2 = New Timer(components)
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 10
+        ' 
+        ' Timer2
+        ' 
+        Timer2.Enabled = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(450, 69)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(53, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "Label1"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(832, 453)
+        Controls.Add(Label1)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "DinoRunner"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Dino As PictureBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label1 As Label
+End Class
