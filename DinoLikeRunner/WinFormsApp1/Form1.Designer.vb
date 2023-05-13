3cc74a3cc12faf7efe1ab16200029338bb5839ea
Alien551 <xraydron@gmail.com>
2023-05-13 11:23:21 +0300

    Add dino-like runner

---
+++ DinoLikeRunner/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.Font = New Font("Bahnschrift SemiBold SemiConden", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(335, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(295, 39)
+        Label1.TabIndex = 0
+        Label1.Text = "Label1"
+        Label1.TextAlign = ContentAlignment.TopRight
+        ' 
+        ' Label2
+        ' 
+        Label2.BackColor = SystemColors.Control
+        Label2.Font = New Font("Tahoma", 30F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(-1, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(646, 305)
+        Label2.TabIndex = 1
+        Label2.Text = "to Retry" & vbCrLf & "🔄" & vbCrLf & "Press any button"
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(643, 305)
+        Controls.Add(Label1)
+        Controls.Add(Label2)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ DinoLikeRunner/WinFormsApp1/Form1.vb
@@ -0,0 +1,102 @@
+﻿Imports System.Runtime.InteropServices
+Public Class Form1 ' объявление класса формы
+    ' объявление переменных
+    Private WithEvents GameTimer As New Timer ' Таймер для игры
+    Private player As PictureBox, ' Игрок
+            obstaclesList As List(Of PictureBox), ' Список препятсвий
+            score As Double = 0, ' Счет игрока
+            gameOver As Boolean = False, ' Флаг, означающий, что игра
+            gravity As Integer = 10, ' Сила гравитации
+            jumpForce As Integer = 0 ' Сила прыжка
+    Private ReadOnly obstacleSpeed As Integer = 12 ' Скорость движения преград
+    ' импорт метода GetKeyDown
+    <DllImport("User32.dll")>
+    Private Shared Function GetKeyState(nVirtKey As Integer) As Short
+    End Function
+    ' процедура, вызываемая при создании формы
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label2.Hide()
+        ' инициализация игрока
+        player = New PictureBox With {
+            .Size = New Size(50, 50),
+            .Location = New Point(100, Me.Height - 100),
+            .BackColor = Color.Black
+        }
+        Controls.Add(player)
+        ' инициализация списка препятствий
+        obstaclesList = New List(Of PictureBox)
+        ' Запускаем таймер для игры
+        GameTimer.Interval = 30
+        GameTimer.Enabled = True
+    End Sub
+    ' процедура, вызываемая компонентом GameTimer
+    Private Sub GameTimer_Tick(sender As Object, e As EventArgs) Handles GameTimer.Tick
+        Dim i As Integer = 0 ' обработка прыжка и падения игрока
+        If player.Bottom < Me.Height - 50 Then
+            player.Top += gravity
+            gravity += 2
+        End If
+        If GetKeyState(Keys.Space) < 0 And player.Bottom >= Me.Height - 50 Then
+            jumpForce = -30
+            gravity = 4
+        End If
+        If jumpForce < 0 Then
+            player.Top += jumpForce
+            jumpForce += 1
+        End If
+        If player.Bottom > Me.Height - 50 Then
+            player.Top = Me.Height - 100
+            gravity = 0
+            jumpForce = 0
+        End If
+        ' смещение препятствий влево
+        For Each obstacle In obstaclesList
+            obstacle.Left -= obstacleSpeed
+            ' Проверяем коллизию игрока с препятствиями
+            If player.Bounds.IntersectsWith(obstacle.Bounds) Then
+                EndGame()
+                Exit Sub
+            End If
+            ' Если препятствие ушло за пределы экрана, то удаляем из списка и с экрана
+            If obstacle.Right < 0 Then
+                Controls.Remove(obstacle)
+                obstaclesList.Remove(obstacle)
+                Exit For ' Выходим из цикла, чтобы избежать ошибки, когда мы удаляем элемент из списка, по которому идет цикл
+            End If
+        Next
+        ' генерация новых препятствий с некоторой вероятностью
+        If Int(score) Mod 10 = 0 And Rnd() < 0.23 And obstaclesList.Count < 3 Then
+            Dim obstacle As New PictureBox With {
+                .Size = New Size(30, 50),
+                .Location = New Point(Me.Width, Me.Height - player.Height - 50),
+                .BackColor = Color.Red
+            }
+            Controls.Add(obstacle)
+            obstaclesList.Add(obstacle)
+        End If
+        ' инкремент счета и вывод на экран
+        score += 0.1
+        Label1.Text = $"{score:000}"
+    End Sub
+    ' процедура, вызываемая при нажатии на любую клавишу
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode And gameOver Then
+            ' начало новой игры, если игра окончена
+            For Each obstacle In obstaclesList
+                Controls.Remove(obstacle)
+            Next
+            obstaclesList.Clear()
+            GameTimer.Enabled = True
+            score = 0
+            gameOver = False
+            Label2.Hide()
+        End If
+    End Sub
+    '
+    Private Sub EndGame()
+        ' завершение игры и вывод сообщения о проигрыше
+        GameTimer.Enabled = False
+        gameOver = True
+        Label2.Show()
+    End Sub
+End Class
